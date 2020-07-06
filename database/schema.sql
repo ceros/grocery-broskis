a221e5fd8887a78d1bea77ee2c65c763ff6df03a
Daniel Bingham <daniel.bingham@ceros.com>
2020-07-06 11:21:08 -0400

    First pass at a database schema.

--- database/schema.sql
+++ database/schema.sql
@@ -2,15 +2,37 @@
 
 CREATE TABLE `users` (
     `id`    BIGINT unsigned NOT NULL AUTO_INCREMENT,
-    `name`  VARCHAR(256),
-    `password`  VARCHAR(64),
-    `email`     VARCHAR(256),
-    `created_date`  DATETIME,
+    `name`  VARCHAR(256) NOT NULL,
+    `password`  VARCHAR(64) NOT NULL,
+    `email`     VARCHAR(256) NOT NULL,
+    `created_date`  DATETIME NOT NULL,
     `updated_date`  DATETIME,
     PRIMARY KEY (`id`),
     INDEX `name` (`name`)
 );
 
-    
+CREATE TABLE `lists` (
+    `id` BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT unsigned NOT NULL,
+    `address`   VARCHAR(512) NOT NULL,
+    `status`    VARCHAR(64) NOT NULL, 
+    `budget`    DECIMAL(15,2),
+    `created_date`  DATETIME NOT NULL,
+    `update_date`   DATETIME,
+    PRIMARY KEY (`id`),
+    INDEX `user_id` (`user_id`)
+
+);
+
+CREATE TABLE `items` (
+    `id` BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    `list_id`   BIGINT unsigned NOT NULL,
+    `description`   VARCHAR(256) NOT NULL,
+    `replaceable`   BOOLEAN NOT NULL,
+    `created_date`  DATETIME NOT NULL,
+    `update_date`   DATETIME,
+    PRIMARY KEY (`id`),
+    INDEX `list_id` (`list_id`)
+);
 
 
