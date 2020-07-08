eb8ef5c65cd92f6117aa7a1a91ed11b7b7543a40
Jeferson Amorim <jeferson.amorim@ceros.com>
2020-07-08 16:10:46 -0300

    Adding action to handle current user

--- client/reducers/users.js
+++ client/reducers/users.js
@@ -1,10 +1,17 @@
-import { RECIEVE_USER } from '../actions/users.js';
+import { RECIEVE_USER, RECIEVE_CURRENT_USER } from '../actions/users.js';
 
 export default function users(state = {}, action) {
     switch (action.type) {
         case RECIEVE_USER:
             return Object.assign({}, state, { [action.user.id]: action.user });
+		case RECIEVE_CURRENT_USER:
+			return {
+                ...state,
+                current: action.user
+            }
         default:
             return state;
     }
 }
+
+export const getCurrentUser = function(state) { return state.users.current };
