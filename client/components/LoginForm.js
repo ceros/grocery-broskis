6ea7d194ce85fe1f76d4d6b6c90efd078954f9a8
Jeferson Amorim <jeferson.amorim@ceros.com>
2020-07-08 19:09:10 -0300

    Applying material

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,53 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {history} from '../helpers/history.js';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class LoginForm extends React.Component { 
 
     constructor(props) {
         super(props);
-        this.state = {
-            email: '',
-            password: '',
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    handleInputChange(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-
-        this.setState({
-            [name]:  value
-        });
     }
 
     onSubmit(event) {
         event.preventDefault();
-        this.props.onSubmit(this.state);
+
+		const formData = {};
+        for (const input of event.target) {
+          if (input.name) {
+            formData[input.name] = input.value;
+          }
+        }
+        this.props.onSubmit(formData);
     }
 
     render() {
         return (
-            <form onSubmit={this.onSubmit}>
-                <label htmlFor="email">Email:</label>
-                <input type="text" 
-                    name="email" 
-                    value={this.state.email}
-                    onChange={this.handleInputChange} />
-                <label htmlFor="password">Password:</label>
-                <input type="password" 
-                    name="password" 
-                    value={this.state.password}
-                    onChange={this.handleInputChange} />
-				<input type="submit" name="signin" value="Sign in" />
-				<button
-					type="button"
-    				onClick={() => history.push('/register')}>
-    				Register
-  				</button>
+            <form onSubmit={this.onSubmit} className="login-form">
+                <TextField className="input" type="text" name="email" label="Email" />
+                <TextField className="input" type="password" name="password" label="Password" />
+                <Button type="submit" color="primary" variant="contained">Login</Button>
+				<Button type="button" onClick={() => history.push('/register')}>Register</Button>
             </form>
         );
     }
