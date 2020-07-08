5d0d5ab0f8fb0dc8505fc6ef10f3900fe5a536be
Jeferson Amorim <jeferson.amorim@ceros.com>
2020-07-08 19:11:59 -0300

    Adding a common place for header functions

---
+++ client/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import {getMe} from '../actions/users.js';
+import {getCurrentUser} from '../reducers/users.js';
+
+class Header extends React.Component { 
+
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+	componentDidMount() {
+        this.props.getMe();
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state);
+    }
+
+    render() {
+		const {user} = this.props;
+
+		if(!user.name) {
+			return <h1>Grocery</h1>;
+		}
+
+        return (
+            <form onSubmit={this.onSubmit} className="logout-form">
+				<h1>Grocery</h1>
+				<div className="user">
+					<p>Wellcome <strong>{user.name}</strong> | <input type="submit" name="logout" value="Logout" /></p>
+				</div>
+            </form>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+	return {
+    	user: getCurrentUser(state)
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({getMe: getMe}, dispatch);
+}
+
+const header = connect(mapStateToProps,mapDispatchToProps)(Header);
+
+export default header;
