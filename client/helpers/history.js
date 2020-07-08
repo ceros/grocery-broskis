a1b4a290cf07171dc16aeaf56cecde9a2d04aad3
Jeferson Amorim <jeferson.amorim@ceros.com>
2020-07-08 19:10:01 -0300

    Fixing unit tests

--- client/helpers/history.js
+++ client/helpers/history.js
@@ -1,3 +1,11 @@
 import { createBrowserHistory } from 'history';
+//
+let history = {}
 
-export const history = createBrowserHistory();
+// It's required to keep tests running
+if (typeof document !== 'undefined') {
+	console.log('testing');
+  	history = createBrowserHistory()
+}
+
+export { history }
