89a5435f4fbd693cfb644da8ee98416fa4fcca1a
Aaron Wood <aaron.wood@ceros.com>
2020-07-09 15:24:27 -0400

    some styling fixes

--- client/components/RegistrationForm.js
+++ client/components/RegistrationForm.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Grid from '@material-ui/core/Grid';
 import {withRouter} from 'react-router';
 
 class RegistrationForm extends React.Component { 
-
     constructor(props) {
         super(props);
 
@@ -35,13 +35,43 @@ class RegistrationForm extends React.Component {
 
     render() {
         return (
+          <div className="login">
+          <h1 className="main-header">Groceros</h1>
+          <Grid
+              container
+              direction="column"
+              justify="space-around"
+              alignItems="center"
+          >
             <form onSubmit={this.onSubmit} className="registration-form">
-                <TextField className="input" type="text" name="name" label="Name" />
-                <TextField className="input" type="text" name="email" label="Email" />
-                <TextField className="input" type="password" name="password" label="Password" />
-                <TextField className="input" type="password" name="confirmPassword" label="Confirm Password" />
-                <Button type="submit" color="primary" variant="contained">Register</Button>
+              <div className="grid-item">
+                <Grid item md>
+                <TextField type="text" name="name" label="Name" />
+                </Grid>
+              </div>
+              <div className="grid-item">
+                <Grid item md>
+                <TextField type="text" name="email" label="Email" />
+                </Grid>
+              </div>
+                <div className="grid-item">
+                <Grid item md>
+                <TextField type="password" name="password" label="Password" />
+                </Grid>
+              </div>
+              <div className="grid-item">
+                <Grid item md>
+                <TextField type="password" name="confirmPassword" label="Confirm" />
+                </Grid>
+              </div>
+              <div className="grid-item">
+                <Grid item md>
+                <Button type="submit" variant="outlined" color="inherit" className="buttons" size="large">Submit</Button>
+                </Grid>
+              </div>
             </form>
+          </Grid>
+        </div>
         );
     }
 }
